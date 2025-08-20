cc24f653ad80221dd07d55c8327b30dc4eff253a
RubMaker <12312030@mail.sustech.edu.cn>
2025-08-20 22:02:39 +0800

    add func dmg appimage

--- xmake/plugins/pack/appimage/main.lua
+++ xmake/plugins/pack/appimage/main.lua
@@ -17,3 +17,287 @@
 -- @author      RubMaker
 -- @file        main.lua
 --
+
+-- imports
+import("core.base.option")
+import("core.base.semver")
+import("core.base.hashset")
+import("lib.detect.find_tool")
+import("lib.detect.find_file")
+import("utils.archive")
+import(".batchcmds")
+
+-- get the appimagetool
+function _get_appimagetool()
+    local appimagetool = find_tool("appimagetool")
+    if not appimagetool then
+        -- try to download appimagetool if not found
+        local appimagetool_url = "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+        local appimagetool_path = path.join(os.tmpdir(), "appimagetool")
+        if not os.isfile(appimagetool_path) then
+            print("appimagetool not found, downloading...")
+            os.runv("wget", {"-O", appimagetool_path, appimagetool_url})
+            os.runv("chmod", {"+x", appimagetool_path})
+        end
+        appimagetool = {program = appimagetool_path}
+    end
+    assert(appimagetool, "appimagetool not found and failed to download!")
+    return appimagetool
+end
+
+-- get linuxdeploy tool
+function _get_linuxdeploy()
+    local linuxdeploy = find_tool("linuxdeploy")
+    if not linuxdeploy then
+        local linuxdeploy_url = "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
+        local linuxdeploy_path = path.join(os.tmpdir(), "linuxdeploy")
+        if not os.isfile(linuxdeploy_path) then
+            print("linuxdeploy not found, downloading...")
+            os.runv("wget", {"-O", linuxdeploy_path, linuxdeploy_url})
+            os.runv("chmod", {"+x", linuxdeploy_path})
+        end
+        linuxdeploy = {program = linuxdeploy_path}
+    end
+    return linuxdeploy
+end
+
+-- get appimage output file
+function _get_appimage_file(package)
+    local filename = string.format("%s-%s-x86_64.AppImage", package:name(), package:version())
+    return path.absolute(path.join(path.directory(package:outputfile() or ""), filename))
+end
+
+-- translate the file path for AppDir structure
+function _translate_filepath(package, filepath, appdir)
+    local prefix = package:get("prefixdir") or "/usr"
+    local relative_path = filepath
+    if filepath:startswith(package:install_rootdir()) then
+        relative_path = path.relative(filepath, package:install_rootdir())
+    end
+    
+    -- map standard directories to AppDir structure
+    if relative_path:startswith("usr/bin/") then
+        return path.join(appdir, "usr/bin", path.filename(relative_path))
+    elseif relative_path:startswith("usr/lib/") then
+        return path.join(appdir, "usr/lib", path.relative(relative_path, "usr/lib"))
+    elseif relative_path:startswith("usr/share/") then
+        return path.join(appdir, "usr/share", path.relative(relative_path, "usr/share"))
+    else
+        return path.join(appdir, relative_path)
+    end
+end
+
+-- get install command for AppDir
+function _get_customcmd(package, appdir, installcmds, cmd)
+    local opt = cmd.opt or {}
+    local kind = cmd.kind
+    if kind == "cp" then
+        local srcfiles = os.files(cmd.srcpath)
+        for _, srcfile in ipairs(srcfiles) do
+            local dstfile = _translate_filepath(package, cmd.dstpath, appdir)
+            if #srcfiles > 1 or path.islastsep(dstfile) then
+                if opt.rootdir then
+                    dstfile = path.join(dstfile, path.relative(srcfile, opt.rootdir))
+                else
+                    dstfile = path.join(dstfile, path.filename(srcfile))
+                end
+            end
+            table.insert(installcmds, string.format("install -Dpm0755 \"%s\" \"%s\"", srcfile, dstfile))
+        end
+    elseif kind == "rm" then
+        local filepath = _translate_filepath(package, cmd.filepath, appdir)
+        table.insert(installcmds, string.format("rm -f \"%s\"", filepath))
+    elseif kind == "rmdir" then
+        local dir = _translate_filepath(package, cmd.dir, appdir)
+        table.insert(installcmds, string.format("rm -rf \"%s\"", dir))
+    elseif kind == "mv" then
+        local srcpath = _translate_filepath(package, cmd.srcpath, appdir)
+        local dstpath = _translate_filepath(package, cmd.dstpath, appdir)
+        table.insert(installcmds, string.format("mv \"%s\" \"%s\"", srcpath, dstpath))
+    elseif kind == "cd" then
+        local dir = _translate_filepath(package, cmd.dir, appdir)
+        table.insert(installcmds, string.format("cd \"%s\"", dir))
+    elseif kind == "mkdir" then
+        local dir = _translate_filepath(package, cmd.dir, appdir)
+        table.insert(installcmds, string.format("mkdir -p \"%s\"", dir))
+    elseif cmd.program then
+        local argv = {}
+        for _, arg in ipairs(cmd.argv) do
+            if path.instance_of(arg) then
+                arg = arg:clone():set(_translate_filepath(package, arg:rawstr(), appdir)):str()
+            elseif path.is_absolute(arg) then
+                arg = _translate_filepath(package, arg, appdir)
+            end
+            table.insert(argv, arg)
+        end
+        table.insert(installcmds, string.format("%s", os.args(table.join(cmd.program, argv))))
+    end
+end
+
+-- get install commands for AppDir
+function _get_installcmds(package, appdir, installcmds, cmds)
+    for _, cmd in ipairs(cmds) do
+        _get_customcmd(package, appdir, installcmds, cmd)
+    end
+end
+
+-- create desktop file
+function _create_desktop_file(package, appdir)
+    local desktop_content = string.format([[
+[Desktop Entry]
+Type=Application
+Name=%s
+Comment=%s
+Exec=%s
+Icon=%s
+Categories=%s
+Version=%s
+]], 
+        package:get("title") or package:name(),
+        package:get("description") or package:get("title") or package:name(),
+        package:name(),
+        package:name(),
+        package:get("category") or "Utility",
+        package:version()
+    )
+    
+    local desktop_file = path.join(appdir, package:name() .. ".desktop")
+    io.writefile(desktop_file, desktop_content)
+    return desktop_file
+end
+
+-- create AppRun script
+function _create_apprun(package, appdir)
+    local main_executable = package:get("bindir") and path.join("usr/bin", package:name()) or package:name()
+    local apprun_content = string.format([[#!/bin/bash
+HERE="$(dirname "$(readlink -f "${0}")")"
+export PATH="${HERE}/usr/bin:${PATH}"
+export LD_LIBRARY_PATH="${HERE}/usr/lib:${LD_LIBRARY_PATH}"
+export XDG_DATA_DIRS="${HERE}/usr/share:${XDG_DATA_DIRS}"
+
+exec "${HERE}/%s" "$@"
+]], main_executable)
+    
+    local apprun_file = path.join(appdir, "AppRun")
+    io.writefile(apprun_file, apprun_content)
+    os.runv("chmod", {"+x", apprun_file})
+    return apprun_file
+end
+
+-- copy icon file
+function _copy_icon(package, appdir)
+    local iconfile = package:get("iconfile")
+    if iconfile and os.isfile(iconfile) then
+        local icon_dst = path.join(appdir, package:name() .. path.extension(iconfile))
+        os.cp(iconfile, icon_dst)
+        return icon_dst
+    else
+        -- create a simple icon if not provided
+        local icon_dst = path.join(appdir, package:name() .. ".png")
+        -- This would need a default icon or skip if no icon provided
+        print("Warning: No icon file specified for AppImage")
+        return nil
+    end
+end
+
+-- pack appimage package
+function _pack_appimage(appimagetool, package)
+    -- create temporary AppDir
+    local appdir_name = package:name() .. ".AppDir"
+    local appdir = path.join(os.tmpdir(), appdir_name)
+    os.tryrm(appdir)
+    os.mkdir(appdir)
+    os.mkdir(path.join(appdir, "usr"))
+    os.mkdir(path.join(appdir, "usr/bin"))
+    os.mkdir(path.join(appdir, "usr/lib"))
+    os.mkdir(path.join(appdir, "usr/share"))
+
+    -- install files to AppDir
+    local prefixdir = package:get("prefixdir")
+    package:set("prefixdir", path.join(appdir, "usr"))
+    
+    local installcmds = {}
+    _get_installcmds(package, appdir, installcmds, batchcmds.get_installcmds(package):cmds())
+    for _, component in table.orderpairs(package:components()) do
+        if component:get("default") ~= false then
+            _get_installcmds(package, appdir, installcmds, batchcmds.get_installcmds(component):cmds())
+        end
+    end
+    
+    -- execute install commands
+    for _, cmd in ipairs(installcmds) do
+        print("Executing: " .. cmd)
+        os.exec(cmd)
+    end
+    
+    package:set("prefixdir", prefixdir)
+
+    -- copy source files
+    local srcfiles, dstfiles = package:sourcefiles()
+    for idx, srcfile in ipairs(srcfiles) do
+        local dstfile = _translate_filepath(package, dstfiles[idx], appdir)
+        os.vcp(srcfile, dstfile)
+    end
+    
+    for _, component in table.orderpairs(package:components()) do
+        if component:get("default") ~= false then
+            local srcfiles, dstfiles = component:sourcefiles()
+            for idx, srcfile in ipairs(srcfiles) do
+                local dstfile = _translate_filepath(package, dstfiles[idx], appdir)
+                os.vcp(srcfile, dstfile)
+            end
+        end
+    end
+
+    -- create required AppImage files
+    _create_desktop_file(package, appdir)
+    _create_apprun(package, appdir)
+    _copy_icon(package, appdir)
+
+    -- use linuxdeploy for dependency resolution if available
+    local linuxdeploy = _get_linuxdeploy()
+    if linuxdeploy then
+        local main_executable = path.join(appdir, "usr/bin", package:name())
+        if os.isfile(main_executable) then
+            print("Using linuxdeploy for dependency resolution...")
+            os.vrunv(linuxdeploy.program, {
+                "--appdir", appdir,
+                "--executable", main_executable,
+                "--desktop-file", path.join(appdir, package:name() .. ".desktop")
+            })
+        end
+    end
+
+    -- build AppImage
+    local appimage_file = _get_appimage_file(package)
+    os.tryrm(appimage_file)
+    
+    -- set ARCH environment variable
+    local arch = package:get("arch") or "x86_64"
+    local envs = {ARCH = arch}
+    
+    print(string.format("Building AppImage: %s", appimage_file))
+    os.vrunv(appimagetool.program, {appdir, appimage_file}, {envs = envs})
+
+    -- copy AppImage file to output location
+    if package:outputfile() then
+        os.vcp(appimage_file, package:outputfile())
+    end
+
+    -- cleanup
+    os.tryrm(appdir)
+end
+
+function main(package)
+    if not is_host("linux") then
+        return
+    end
+
+    cprint("packing %s", package:outputfile() or _get_appimage_file(package))
+
+    -- get appimagetool
+    local appimagetool = _get_appimagetool()
+
+    -- pack appimage
+    _pack_appimage(appimagetool, package)
+end
--- xmake/plugins/pack/dmg/main.lua
+++ xmake/plugins/pack/dmg/main.lua
@@ -17,3 +17,373 @@
 -- @author      RubMaker
 -- @file        main.lua
 --
+
+-- imports
+import("core.base.option")
+import("core.base.semver")
+import("core.base.hashset")
+import("lib.detect.find_tool")
+import("lib.detect.find_file")
+import("utils.archive")
+import(".batchcmds")
+
+-- get hdiutil tool
+function _get_hdiutil()
+    local hdiutil = find_tool("hdiutil", {force = true})
+    assert(hdiutil, "hdiutil not found, DMG creation requires macOS!")
+    return hdiutil
+end
+
+-- get codesign tool (optional)
+function _get_codesign()
+    return find_tool("codesign", {force = false})
+end
+
+-- get create-dmg tool (optional, for better DMG creation)
+function _get_create_dmg()
+    return find_tool("create-dmg", {force = false})
+end
+
+-- get dmg file path
+function _get_dmgfile(package)
+    return path.absolute(path.join(path.directory(package:sourcedir()), package:name() .. "-" .. package:version() .. ".dmg"))
+end
+
+-- translate the file path for macOS
+function _translate_filepath(package, filepath)
+    return filepath:replace(package:install_rootdir(), "/Applications", {plain = true})
+end
+
+-- get install command for macOS
+function _get_customcmd(package, installcmds, cmd)
+    local opt = cmd.opt or {}
+    local kind = cmd.kind
+    if kind == "cp" then
+        local srcfiles = os.files(cmd.srcpath)
+        for _, srcfile in ipairs(srcfiles) do
+            -- the destination is directory? append the filename
+            local dstfile = _translate_filepath(package, cmd.dstpath)
+            if #srcfiles > 1 or path.islastsep(dstfile) then
+                if opt.rootdir then
+                    dstfile = path.join(dstfile, path.relative(srcfile, opt.rootdir))
+                else
+                    dstfile = path.join(dstfile, path.filename(srcfile))
+                end
+            end
+            table.insert(installcmds, string.format("cp -R \"%s\" \"%s\"", srcfile, dstfile))
+        end
+    elseif kind == "rm" then
+        local filepath = _translate_filepath(package, cmd.filepath)
+        table.insert(installcmds, string.format("rm -f \"%s\"", filepath))
+    elseif kind == "rmdir" then
+        local dir = _translate_filepath(package, cmd.dir)
+        table.insert(installcmds, string.format("rm -rf \"%s\"", dir))
+    elseif kind == "mv" then
+        local srcpath = _translate_filepath(package, cmd.srcpath)
+        local dstpath = _translate_filepath(package, cmd.dstpath)
+        table.insert(installcmds, string.format("mv \"%s\" \"%s\"", srcpath, dstpath))
+    elseif kind == "cd" then
+        local dir = _translate_filepath(package, cmd.dir)
+        table.insert(installcmds, string.format("cd \"%s\"", dir))
+    elseif kind == "mkdir" then
+        local dir = _translate_filepath(package, cmd.dir)
+        table.insert(installcmds, string.format("mkdir -p \"%s\"", dir))
+    elseif cmd.program then
+        local argv = {}
+        for _, arg in ipairs(cmd.argv) do
+            if path.instance_of(arg) then
+                arg = arg:clone():set(_translate_filepath(package, arg:rawstr())):str()
+            elseif path.is_absolute(arg) then
+                arg = _translate_filepath(package, arg)
+            end
+            table.insert(argv, arg)
+        end
+        table.insert(installcmds, string.format("%s", os.args(table.join(cmd.program, argv))))
+    end
+end
+
+-- get build commands
+function _get_buildcmds(package, buildcmds, cmds)
+    for _, cmd in ipairs(cmds) do
+        _get_customcmd(package, buildcmds, cmd)
+    end
+end
+
+-- get install commands
+function _get_installcmds(package, installcmds, cmds)
+    for _, cmd in ipairs(cmds) do
+        _get_customcmd(package, installcmds, cmd)
+    end
+end
+
+-- get specvars for DMG
+function _get_specvars(package)
+    local specvars = table.clone(package:specvars())
+    specvars.PACKAGE_DATE = os.date("%Y-%m-%d %H:%M:%S")
+    local author = package:get("author") or "Unknown Developer"
+    specvars.PACKAGE_COPYRIGHT = "Copyright © " .. os.date("%Y") .. " " .. author
+    specvars.PACKAGE_IDENTIFIER = package:get("identifier") or ("com.example." .. package:name())
+    specvars.PACKAGE_BUNDLE_VERSION = package:version()
+    specvars.PACKAGE_BUNDLE_SHORT_VERSION = package:version()
+    
+    -- Get codesign identity if available
+    local identity = package:get("codesign_identity") or os.getenv("CODESIGN_IDENTITY")
+    specvars.CODESIGN_IDENTITY = identity or ""
+    
+    -- DMG appearance settings
+    specvars.DMG_WINDOW_X = package:get("dmg_window_x") or 100
+    specvars.DMG_WINDOW_Y = package:get("dmg_window_y") or 100  
+    specvars.DMG_WINDOW_WIDTH = package:get("dmg_window_width") or 540
+    specvars.DMG_WINDOW_HEIGHT = package:get("dmg_window_height") or 380
+    specvars.DMG_BACKGROUND = package:get("dmg_background") or ""
+    specvars.DMG_ICON_SIZE = package:get("dmg_icon_size") or 80
+    
+    return specvars
+end
+
+-- create app bundle structure
+function _create_app_bundle(package, bundle_dir)
+    -- Create basic app bundle structure
+    local app_name = package:name() .. ".app"
+    local app_path = path.join(bundle_dir, app_name)
+    local contents_dir = path.join(app_path, "Contents")
+    local macos_dir = path.join(contents_dir, "MacOS")
+    local resources_dir = path.join(contents_dir, "Resources")
+    
+    os.mkdir(contents_dir)
+    os.mkdir(macos_dir)
+    os.mkdir(resources_dir)
+    
+    -- Copy executable and resources
+    local srcfiles, dstfiles = package:sourcefiles()
+    for idx, srcfile in ipairs(srcfiles) do
+        local dstfile = dstfiles[idx]
+        if dstfile:find("MacOS") then
+            dstfile = path.join(macos_dir, path.filename(dstfile))
+        elseif dstfile:find("Resources") then
+            dstfile = path.join(resources_dir, path.filename(dstfile))
+        else
+            dstfile = path.join(contents_dir, path.relative(dstfile, package:install_rootdir()))
+        end
+        os.vcp(srcfile, dstfile)
+        
+        -- Make executable files executable
+        if dstfile:find("MacOS") then
+            os.runv("chmod", {"+x", dstfile})
+        end
+    end
+    
+    -- Copy component files
+    for _, component in table.orderpairs(package:components()) do
+        if component:get("default") ~= false then
+            local srcfiles, dstfiles = component:sourcefiles()
+            for idx, srcfile in ipairs(srcfiles) do
+                local dstfile = dstfiles[idx]
+                if dstfile:find("MacOS") then
+                    dstfile = path.join(macos_dir, path.filename(dstfile))
+                elseif dstfile:find("Resources") then
+                    dstfile = path.join(resources_dir, path.filename(dstfile))
+                else
+                    dstfile = path.join(contents_dir, path.relative(dstfile, package:install_rootdir()))
+                end
+                os.vcp(srcfile, dstfile)
+                
+                if dstfile:find("MacOS") then
+                    os.runv("chmod", {"+x", dstfile})
+                end
+            end
+        end
+    end
+    
+    -- Generate Info.plist
+    _generate_info_plist(package, path.join(contents_dir, "Info.plist"))
+    
+    return app_path
+end
+
+-- generate Info.plist file
+function _generate_info_plist(package, plist_path)
+    local specvars = _get_specvars(package)
+    local plist_content = string.format([[<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>CFBundleExecutable</key>
+    <string>%s</string>
+    <key>CFBundleIdentifier</key>
+    <string>%s</string>
+    <key>CFBundleName</key>
+    <string>%s</string>
+    <key>CFBundleDisplayName</key>
+    <string>%s</string>
+    <key>CFBundleVersion</key>
+    <string>%s</string>
+    <key>CFBundleShortVersionString</key>
+    <string>%s</string>
+    <key>CFBundlePackageType</key>
+    <string>APPL</string>
+    <key>CFBundleSignature</key>
+    <string>????</string>
+    <key>NSHighResolutionCapable</key>
+    <true/>
+    <key>NSHumanReadableCopyright</key>
+    <string>%s</string>
+</dict>
+</plist>]], 
+        package:name(),
+        specvars.PACKAGE_IDENTIFIER,
+        package:name(),
+        package:displayname() or package:name(),
+        specvars.PACKAGE_BUNDLE_VERSION,
+        specvars.PACKAGE_BUNDLE_SHORT_VERSION,
+        specvars.PACKAGE_COPYRIGHT
+    )
+    
+    io.writefile(plist_path, plist_content)
+end
+
+-- code sign the app bundle
+function _codesign_bundle(codesign, app_path, identity)
+    if codesign and identity and identity ~= "" then
+        cprint("Code signing %s with identity: %s", path.filename(app_path), identity)
+        os.vrunv(codesign.program, {
+            "--force",
+            "--sign", identity,
+            "--timestamp",
+            "--options", "runtime",
+            app_path
+        })
+    end
+end
+
+-- create DMG using hdiutil
+function _create_dmg_hdiutil(hdiutil, package, bundle_dir, dmg_file)
+    local temp_dmg = dmg_file .. ".temp.dmg"
+    local volume_name = package:displayname() or package:name()
+    
+    -- Create temporary DMG
+    cprint("Creating temporary DMG...")
+    os.vrunv(hdiutil.program, {
+        "create",
+        "-srcfolder", bundle_dir,
+        "-volname", volume_name,
+        "-fs", "HFS+",
+        "-fsargs", "-c c=64,a=16,e=16",
+        "-format", "UDRW",
+        temp_dmg
+    })
+    
+    -- Mount the temporary DMG
+    cprint("Mounting DMG for customization...")
+    local mount_output = os.iorunv(hdiutil.program, {"attach", "-readwrite", "-noverify", temp_dmg})
+    local mount_point = mount_output:match("/Volumes/[^\r\n]*")
+    
+    if mount_point then
+        -- Create Applications symlink
+        os.runv("ln", {"-sf", "/Applications", path.join(mount_point, "Applications")})
+        
+        -- Copy background image if provided
+        local background = package:get("dmg_background")
+        if background and os.isfile(background) then
+            local background_dir = path.join(mount_point, ".background")
+            os.mkdir(background_dir)
+            os.cp(background, path.join(background_dir, path.filename(background)))
+        end
+        
+        -- Unmount
+        os.vrunv(hdiutil.program, {"detach", mount_point})
+    end
+    
+    -- Convert to final compressed DMG
+    cprint("Creating final compressed DMG...")
+    os.tryrm(dmg_file)
+    os.vrunv(hdiutil.program, {
+        "convert", temp_dmg,
+        "-format", "UDZO",
+        "-imagekey", "zlib-level=9",
+        "-o", dmg_file
+    })
+    
+    -- Clean up
+    os.tryrm(temp_dmg)
+end
+
+-- create DMG using create-dmg tool (if available)
+function _create_dmg_advanced(create_dmg, package, bundle_dir, dmg_file)
+    local volume_name = package:displayname() or package:name()
+    local specvars = _get_specvars(package)
+    local args = {
+        "--volname", volume_name,
+        "--window-size", specvars.DMG_WINDOW_WIDTH, specvars.DMG_WINDOW_HEIGHT,
+        "--icon-size", specvars.DMG_ICON_SIZE,
+        "--app-drop-link", "450", "150"
+    }
+    
+    -- Add background if specified
+    if specvars.DMG_BACKGROUND ~= "" and os.isfile(specvars.DMG_BACKGROUND) then
+        table.insert(args, "--background")
+        table.insert(args, specvars.DMG_BACKGROUND)
+    end
+    
+    table.insert(args, dmg_file)
+    table.insert(args, bundle_dir)
+    
+    cprint("Creating DMG with create-dmg...")
+    os.vrunv(create_dmg.program, args)
+end
+
+-- pack DMG package
+function _pack_dmg(hdiutil, codesign, create_dmg, package)
+    local dmg_file = _get_dmgfile(package)
+    local bundle_dir = path.join(os.tmpdir(), "dmg_build_" .. package:name())
+    
+    -- Clean and create bundle directory
+    os.tryrm(bundle_dir)
+    os.mkdir(bundle_dir)
+    
+    -- Create app bundle
+    local app_path = _create_app_bundle(package, bundle_dir)
+    
+    -- Code sign if identity is provided
+    local specvars = _get_specvars(package)
+    if specvars.CODESIGN_IDENTITY ~= "" then
+        _codesign_bundle(codesign, app_path, specvars.CODESIGN_IDENTITY)
+    end
+    
+    -- Create DMG
+    if create_dmg then
+        _create_dmg_advanced(create_dmg, package, bundle_dir, dmg_file)
+    else
+        _create_dmg_hdiutil(hdiutil, package, bundle_dir, dmg_file)
+    end
+    
+    -- Copy to output location
+    os.vcp(dmg_file, package:outputfile())
+    
+    -- Clean up
+    os.tryrm(bundle_dir)
+    os.tryrm(dmg_file)
+end
+
+function main(package)
+    if not is_host("macosx") then
+        return
+    end
+
+    cprint("packing %s", package:outputfile())
+
+    -- Get required tools
+    local hdiutil = _get_hdiutil()
+    local codesign = _get_codesign()
+    local create_dmg = _get_create_dmg()
+    
+    if create_dmg then
+        cprint("Using create-dmg for enhanced DMG creation")
+    else
+        cprint("Using hdiutil for basic DMG creation")
+        cprint("Install create-dmg for better DMG appearance: brew install create-dmg")
+    end
+
+    -- Pack DMG package
+    _pack_dmg(hdiutil, codesign, create_dmg, package)
+end
