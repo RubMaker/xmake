eb9d3a9acd9557c02afc55000155effac260216f
RubMaker <12312030@mail.sustech.edu.cn>
2025-09-02 17:10:08 +0800

    feat hdiutil

--- xmake/plugins/pack/dmg/main.lua
+++ xmake/plugins/pack/dmg/main.lua
@@ -29,13 +29,9 @@ import(".batchcmds")
 
 -- get the hdiutil tool
 function _get_hdiutil()
-    local hdiutil = find_tool("hdiutil")
-    if not hdiutil then
-        hdiutil_path = "/usr/bin/hdiutil"
-        if os.isfile(hdiutil_path) then
-            hdiutil = {program = hdiutil_path}
-            assert(hdiutil, "hdiutil not found! DMG packaging requires macOS system tools.!!!!!!!!")
-        end
+    local hdiutil_path = "/usr/bin/hdiutil"
+    if os.isfile(hdiutil_path) then
+        hdiutil = {program = hdiutil_path}
     end
     assert(hdiutil, "hdiutil not found! DMG packaging requires macOS system tools.")
     return hdiutil
