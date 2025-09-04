662cc2a1a3c5ec26c98c9bb0e4a22bfc36c685e2
RubMaker <12312030@mail.sustech.edu.cn>
2025-09-04 15:01:58 +0800

    dmg mod

--- xmake/plugins/pack/dmg/main.lua
+++ xmake/plugins/pack/dmg/main.lua
@@ -343,18 +343,17 @@ function _pack_dmg_main(hdiutil, create_dmg, package)
     
     -- 创建临时工作目录
     local dmg_staging_dir = path.join(os.tmpdir(), package:name() .. "_dmg_staging")
-    os.runv("rm", {"-rf", dmg_staging_dir})
+    os.runv(rm, {"-rf", dmg_staging_dir})
     -- 创建新的临时目录
-    local tmpok, err_message = os.mkdir(dmg_staging_dir)
+    local tmpok = os.runv(mkdir, {"-p", dmg_staging_dir})
     if not tmpok then
         print("Error: Failed to create temporary staging directory.")
-        print("Details:", err_message)
         print("mkdir_tmp_dir_ok:", tmpok)
         -- 如果创建失败，立即返回 false，停止后续操作
         return false
     end
 
-    print("mkdir_tmp_dir_ok:", mkdir_tmp_dir_ok)
+    print("mkdir_tmp_dir:", dmg_staging_dir)
 
     -- 复制.app bundle到staging目录
     print("Copying .app bundle to staging directory...")
